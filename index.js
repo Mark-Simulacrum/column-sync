cd932bdaab900eef9f406054ffda0a6609bca5e6
MarkSimulacrum <mark.simulacrum@gmail.com>
2014-10-25 12:04:42 -0600

    Padding, ids must be numbers.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /*jshint browser:true*/
 /*globals _ */
 
-window.onload = function () {
+window.onload = init;
+
+function init() {
 	'use strict';
 
 	var syncDividers = document.querySelectorAll('[sync]');
@@ -22,25 +24,22 @@ window.onload = function () {
 		return null;
 	}
 
-	_.forEach(syncGroups, function (syncGroup) {
+	_.forEach(syncGroups, function (syncGroup, groupID) {
 		var maxOffsetTop = _.max(syncGroup, 'offsetTop').offsetTop;
 
 		_.forEach(syncGroup, function (syncDivider) {
-			syncDivider.innerHTML = 'Group: ' + syncDivider.getAttribute('sync');
-
 			var siblings = _.toArray(syncDivider.parentElement.children);
 			var currentDividerIndex = _.indexOf(siblings, syncDivider);
 			var previousDividerIndex = _.indexOf(siblings, findPreviousDivider(syncDivider));
-			previousDividerIndex = previousDividerIndex < 0 ? 0 : previousDividerIndex + 1; // Add 1 if it exists, otherwise set to 0
+			previousDividerIndex = previousDividerIndex < 0 ? 0 : previousDividerIndex + 1;
 			var elementsAfterPreviousDivider = siblings.slice(previousDividerIndex, currentDividerIndex);
 			var paddingPerElement = (maxOffsetTop - syncDivider.offsetTop) / elementsAfterPreviousDivider.length;
 
-			if (paddingPerElement !== 0) {
-				_.forEach(elementsAfterPreviousDivider, function (element) {
-					element.style.paddingTop = (paddingPerElement / 2) + 'px';
-					element.style.paddingBottom = (paddingPerElement / 2) + 'px';
-				});
-			}
+			syncDivider.innerHTML = 'Group: ' + groupID;
+
+			_.forEach(elementsAfterPreviousDivider, function (element) {
+				element.style.padding = (paddingPerElement / 2) + 'px 0';
+			});
 		});
 	});
-};
+}
