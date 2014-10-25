f2885117c0161ae2807de31926744556f45271db
MarkSimulacrum <mark.simulacrum@gmail.com>
2014-10-25 08:41:36 -0600

    Change to padding elements.

--- index.js
+++ index.js
@@ -4,23 +4,39 @@
 window.onload = function () {
 	'use strict';
 
-	var syncDividers = document.querySelectorAll('[sync]');
+	var syncDividers = _.toArray(document.querySelectorAll('[sync]'));
 	var syncGroups = _.groupBy(syncDividers, function (element) {
 		return element.getAttribute('sync');
 	});
+
+	function findPreviousDivider(element) {
+		while (element.previousElementSibling) {
+			var previousElement = element.previousElementSibling;
+			if (previousElement.hasAttribute('sync')) {
+				return previousElement;
+			} else {
+				return findPreviousDivider(previousElement);
+			}
+		}
+
+		return null;
+	}
 
 	_.forEach(syncGroups, function (syncGroup) {
 		var maxOffsetTop = _.max(syncGroup, 'offsetTop').offsetTop;
 
 		_.forEach(syncGroup, function (syncDivider) {
 			syncDivider.innerHTML = syncDivider.getAttribute('sync') + ' ' + maxOffsetTop;
-			var padding = maxOffsetTop - syncDivider.offsetTop;
-			syncDivider.style.height = padding + 'px';
 
 			var siblings = _.toArray(syncDivider.parentElement.children);
+			var currentDividerIndex = _.indexOf(siblings, syncDivider);
+			var previousDividerIndex = _.indexOf(siblings, findPreviousDivider(syncDivider));
+			var elementsAfterPreviousDivider = siblings.slice((previousDividerIndex === -1) ? 0 : previousDividerIndex + 1, currentDividerIndex);
+			var paddingPerElement = (maxOffsetTop - syncDivider.offsetTop) / elementsAfterPreviousDivider.length;
 
-			_.forEach(siblings, function (sibling) {
-				//sibling.style.paddingTop = padding / siblings.length + 'px';
+			_.forEach(elementsAfterPreviousDivider, function (element) {
+				element.style.paddingTop = (paddingPerElement / 2) + 'px';
+				element.style.paddingBottom = (paddingPerElement / 2) + 'px';
 			});
 		});
 	});
